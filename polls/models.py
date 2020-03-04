4c36a62d13aceb1697e7202e21f403da873e2e45
Pavel <antiplus15@gmail.com>
2020-03-04 14:13:10 +0200

    update polls/model/Question/pub_date

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
